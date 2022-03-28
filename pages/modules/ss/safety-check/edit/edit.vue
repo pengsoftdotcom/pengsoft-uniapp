53acbb2c8dd44e30a15a338f1b51df0aa8901184
peng.dang <peng.dang@pengsoft.com>
2022-03-28 21:14:36 +0800

    init

--- pages/modules/ss/safety-check/edit/edit.vue
+++ pages/modules/ss/safety-check/edit/edit.vue
@@ -8,11 +8,25 @@
 	export default {
 		data() {
 			return {
-				
+				form: {
+					project: null, // 工程项目
+					code: null, // 编码
+					subject: null, // 检查主题
+					checker: null, // 检查人
+					status: null, // 状态
+					submitFiles: null, // 检查图片
+					reason: null, // 检查描述
+					submittedAt: null, // 提交时间
+					handleFiles: null, // 处理图片
+					result: null, // 处理结果
+					handledAt: null // 处理时间
+				}
 			}
 		},
 		methods: {
-			
+			submit() {
+				
+			}
 		}
 	}
 </script>
