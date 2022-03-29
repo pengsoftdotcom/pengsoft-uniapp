42148b17e19a905824c2419aecb95826031d4394
worry <12694525@qq.com>
2022-03-30 00:49:47 +0800

    代办

--- pages/task/task.vue
+++ pages/task/task.vue
@@ -1,6 +1,14 @@
 <template>
 	<view>
-		
+		<view class="w-list-wrap">
+			<view v-for="(item, index) in listData" :key="index" :class="itemClass[item.level]" class="w-list-item" @click="clickItem(item)">
+				<view class="w-list-item-title uni-ellipsis-2">{{ item.title }}</view>
+				<view class="w-list-item-body">
+					<view>{{ item.content }}</view>
+				</view>
+			</view>
+		</view>
+		<uni-load-more :status="status" :icon-size="16" :content-text="contentText" />
 	</view>
 </template>
 
@@ -8,16 +16,34 @@
 	export default {
 		data() {
 			return {
-				
+				itemClass: {
+					'1': 'border-info',
+					'2': 'border-warning',
+					'3': 'border-danger',
+				},
+				...uni.listModel,
 			}
 		},
 		methods: {
-			
 		},
-		onShow() {
-			// uni.navigateTo({
-			// 	url: '../modules/oa/contract/list/list'
-			// })
+		onReady() {
+			this.listData = [{
+					title: 'title',
+					content: 'content',
+					level: '1'
+				},
+				{
+					title: 'title',
+					content: 'content',
+					level: '2'
+				},
+				{
+					title: 'title',
+					content: 'content',
+					level: '3'
+				},
+			]
+			console.log(this)
 		}
 	}
 </script>
